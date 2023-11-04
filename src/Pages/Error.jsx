26ba9bb0f73e2ff4aa9bf1de102e2c5c32d45013
MuhammadSahossKhan <sahosskhan359@gmail.com>
2023-11-04 19:39:06 +0600

    add error page

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -2,7 +2,15 @@
 const Error = () => {
     return (
         <div>
-                       <h1 className='text-red-500 '>welcome to error page</h1> 
+                      <section className="flex items-center h-full p-16 dark:bg-gray-900 dark:text-gray-100">
+	<div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+		<div className="max-w-2xl text-center">
+            <img src="https://i.ibb.co/NWK4LnZ/giphy-gif-cid-6c09b9521zxf4tznvd0bqjjz90cz27ybt54icccc8t7dtjp6-ep-v1-gifs-search-rid-giphy.gif" alt="" />
+			<p className="text-2xl font-semibold md:text-3xl">Sorry, we could not find this page.</p>
+			
+		</div>
+	</div>
+</section>
         </div>
     );
 };
