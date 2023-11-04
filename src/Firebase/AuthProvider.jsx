376fbfdb09bf3c2a88c087d2b234b506964e4cba
MuhammadSahossKhan <sahosskhan359@gmail.com>
2023-11-05 02:17:58 +0600

    fix login by account after register issue

--- src/Firebase/AuthProvider.jsx
+++ src/Firebase/AuthProvider.jsx
@@ -14,12 +14,13 @@ import app from "./firebase.config";
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
+
 // eslint-disable-next-line react/prop-types
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState("");
   const [loading, setLoading] = useState(true);
 
-  // eslint-disable-next-line no-unused-vars
+
   const googleLogin = (value) => {
     setLoading(true);
     return signInWithPopup(auth, provider);
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -72,31 +72,45 @@ const Login = () => {
 <div className="flex  justify-center mx-auto mt-2">
 <h1 className="text-2xl font-bold">Welcome, Login here!!</h1>
 </div>
-    <form onSubmit={HandleLogin} className="mt-6">
-        <div>
-            <label  className="block text-sm text-gray-800 dark:text-gray-200">Email</label>
-            <input type="email" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-red-400 dark:focus:border-red-300 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40" />
-        </div>
+<form onSubmit={HandleLogin}>
+              <div className="mt-4 form-control">
+                <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200">
+                  Email Address
+                </label>
+                <input
+                  id="LoggingEmailAddress"
+                  name="email"
+                  className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
+                  type="email"
+                />
+              </div>
 
-        <div className="mt-4">
-            <div className="flex items-center justify-between">
-                <label  className="block text-sm text-gray-800 dark:text-gray-200">Password</label>
-            </div>
+              <div className="mt-4 form-control">
+                <div className="flex justify-between">
+                  <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200">
+                    Password
+                  </label>
+                </div>
 
-            <input type="password" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-red-400 dark:focus:border-red-300 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40" />
-        </div>
-        {error && (
+                <input
+                  id="loggingPassword"
+                  name="password"
+                  className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
+                  type="password"
+                />
+              </div>
+              {error && (
                 <p className="text-red-700 font-bold mt-5  ">
                   {" "}
                   <i className="fa-solid fa-triangle-exclamation"></i> {error}
                 </p>
               )}
-        <div className="mt-6">
-            <button className="w-full px-6 py-2.5 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
-                Login
-            </button>
-        </div>
-    </form>
+              <div className="mt-6 form-control">
+                <button className="w-full px-6 py-3 text-base font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
+                  LogIn
+                </button>
+              </div>
+            </form>
 
 
     <div className="flex items-center mt-6 -mx-2">
