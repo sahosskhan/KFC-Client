bfdaddb7d229f4e95396078233164e11da3253e2
MuhammadSahossKhan <sahosskhan359@gmail.com>
2023-11-05 01:11:41 +0600

    add firebase authentication env done

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBlMh4YZxuf9GQyEbriJdjKARK9Ypnb_cg",
-  authDomain: "kfc-kushtia-bd.firebaseapp.com",
-  projectId: "kfc-kushtia-bd",
-  storageBucket: "kfc-kushtia-bd.appspot.com",
-  messagingSenderId: "844723514577",
-  appId: "1:844723514577:web:7f5adf125fcab8873fa9f0"
+apiKey: import.meta.env.VITE_APIKEY,
+authDomain: import.meta.env.VITE_AUTHDOMAIN,
+projectId: import.meta.env.VITE_PROJECTID,
+storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
